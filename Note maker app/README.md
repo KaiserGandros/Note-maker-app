777a8378d4386e34c608fce5bee127034d2a5965
Joshua Eagan <140120826+KaiserGandros@users.noreply.github.com>
2023-12-20 15:56:50 -0800

    uploading all base files

---
+++ Note maker app/README.md
@@ -0,0 +1,33 @@
+
+  # Note maker app
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+This is a note taking web application that allows to keep track of information that he/she inputs into it. It uses javascript for its dynamic abilites to display the cards with the title and description info. It also uses node and express.js to use a static .JSON file for information storage using two queries.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Install base files into folder. type 'npm i' in the console to get node modules. run 'node server.js'
+
+## Usage
+To make the tracking of information easier and to assist the user in completing tasks.
+
+## License
+This project is licensed under the MIT license.
+For more information, please visit https://opensource.org/licenses/MIT
+
+## Tests
+3
+
+## Questions
+For any questions, please contact me at [JoshuaPEagan@outlook.com](mailto:JoshuaPEagan@outlook.com).
+
+GitHub: [KaiserGandros](https://github.com/KaiserGandros)
+
---
+++ Note maker app/server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const PORT = 3009;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+function readDB() {
+    const data = fs.readFileSync('./db/db.json', 'utf8');
+    return JSON.parse(data); 
+}
+
+function writeDB(data){
+    fs.writeFileSync('./db/db.json', JSON.stringify(data, null, 2), 'utf8');
+}
+
+app.get('/', (req, res)=>{
+    res.send ('Welcome to the note-taking app');
+})
+
+app.get('/api/notes', (req,res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading file');
+            return;
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+app.post('/api/notes', (req, res) =>{
+    const newNote = req.body;
+    console.log(req.body);
+    if (!newNote.title || !newNote.text) {
+        return res.status(400).send('Both title and text are required');
+    }
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) =>{
+        if (err) {
+            res.status(500).send('Error reading file');
+            return;
+        }   
+        const notes = JSON.parse(data);
+        newNote.id = notes.length + 1;
+        notes.push(newNote);
+        writeDB(notes);
+        res.status(201).send(newNote);
+    });
+});
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
